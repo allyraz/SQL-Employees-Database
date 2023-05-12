64b01a80ab47f9426c6b2d5f7fbe3f67fd1ae4e6
allyraz <124096428+allyraz@users.noreply.github.com>
2023-05-12 13:08:09 -0500

    Update and rename 1. Num of records to 1.SnapshotQueries.sql

--- SQL Queries/1.SnapshotQueries.sql
+++ SQL Queries/1.SnapshotQueries.sql
@@ -17,3 +17,11 @@ union
 select count(*)
 from titles
 ;
+
+# Finding the number of hires in a year
+SELECT 
+    YEAR(hire_date) AS year_date,
+    COUNT(emp_no) AS num_of_employees_dept
+FROM
+    employees
+GROUP BY year_date;
